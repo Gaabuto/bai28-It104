15b1e7415266a4533f7a16993ec0e45416caee51
Gaabuto <khanh040604@gmail.com>
2025-09-15 09:48:26 +0700

    Update bai8.js

--- session28/components/PTIT_CNTT3_IT104_Session28_Bai08/bai8.js
+++ session28/components/PTIT_CNTT3_IT104_Session28_Bai08/bai8.js
@@ -2,12 +2,13 @@ function myFilter(array,number,callback){
     const result = [];
     
     for (let i = 0; i < array.length; i++) {
-            result.push(array[i]);
+            result.push(filter(array[i]));
     }
     return result;
 }
 function filter(number){
     console.log(number);
 }
+
 
 myFilter([1,2,3,4,4,4,4,4,5],4,filter);
